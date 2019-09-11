380b992eaa0a946e28908b42cff5c3be2351bfa8
Bernardo Trevizan <trevizanbernardo@gmail.com>
2019-09-11 13:10:16 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Random Forest
+A classic random forest implementation for Machine Learning class at UFRGS.
+
+#### Note
+In development.
---
+++ src/utils.py
@@ -0,0 +1,48 @@
+import numpy as np
+from math import log2, fsum
+
+
+def entropy(values):
+    """Get the values' entropy (measure of randomness).
+
+    :param values: iterable
+        Categorical values.
+
+    :return: float
+    """
+    counts, total = __unique(values)
+
+    result = 0
+    for n in counts:
+        p = n / total
+        result = result - p * log2(p)
+
+    return result
+
+
+def gini(values):
+    counts, total = __unique(values)
+    result = [pow(n / total, 2) for n in counts]
+    return 1 - fsum(result)
+
+
+def information_gain(a, b, index=entropy):
+    """Calculate the information gain using an index function.
+
+    :param a: iterable
+        Categorical values.
+
+    :param b: iterable
+        Categorical values.
+
+    :param index: list->float (default entropy)
+        Index function (entropy or gini).
+
+    :return: float
+    """
+    return index(a) - index(b)
+
+
+def __unique(values):
+    _, counts = np.unique(values, return_counts=True)
+    return counts, sum(counts)
