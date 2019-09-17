84e98e9eac3e07e520c58057ed9981226c3635d8
Bernardo Trevizan <trevizanbernardo@gmail.com>
2019-09-17 17:25:09 -0300

    load method implemented

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,8 @@
+from pandas import read_csv, concat
+from math import ceil
+from os import path
 import numpy as np
-from math import floor, ceil
+import json
 
 
 def entropy(values):
@@ -33,11 +36,11 @@ def gini(values):
     return 1 - np.sum(result)
 
 
-def information_gain(a, b, index=entropy):
+def information_gain(class_entropy, b, index=entropy):
     """Calculate the information gain using an index function.
 
-    :param a: iterable
-        Categorical values.
+    :param class_entropy: float
+        Target value's entropy.
 
     :param b: iterable
         Categorical values.
@@ -47,7 +50,7 @@ def information_gain(a, b, index=entropy):
 
     :return: float
     """
-    return index(a) - index(b)
+    return class_entropy - index(b)
 
 
 def bootstrap(n, random_state):
@@ -100,6 +103,82 @@ def stratified_split(y, n_splits, random_state):
         train = [index for indexes in folds[0:i] + folds[i+1:] for index in indexes]
 
         yield train, test
+
+
+def cross_validate(model, x, y, k, random_state):
+    """Run a k-fold cross validation.
+
+    :param model: object
+        Object with a fit(x, y) and predict([x]) function.
+
+    :param x: matrix
+        Instance's attributes.
+
+    :param y: list
+        Instance's classes.
+
+    :param k: int
+        Number of folds.
+
+    :param random_state: instance of numpy.random.RandomState
+        Seed for random generator.
+
+    :return: list
+        Test metric for each fold.
+    """
+    metrics = []
+
+    for train_i, test_i in stratified_split(y, k, random_state):
+        x_train = x[train_i, :]
+        y_train = y[train_i]
+
+        x_test = x[test_i, :]
+        y_test = y[test_i]
+
+        model.fit(x_train, y_train)
+        y_pred = model.predict(x_test)
+
+        performance = 0  # TODO: IMPLEMENT F1-SCORE f1_score(y_test, y_pred)
+        metrics.append(performance)
+
+    return metrics
+
+
+def load(dataset):
+    """Load a dataset.
+
+    :param dataset: str
+        Dataset name. Options: credit_g, spambase, vertebra_column, wine
+
+    :return: tuple
+        (metadata, x, y), where metadata is a list of numerical features' indexes, x is a matrix and y is a vector.
+    """
+    default_path = 'tests/datasets/'
+    datasets = ['credit_g', 'spambase', 'vertebra_column', 'wine']
+
+    if dataset not in datasets:
+        raise ValueError('{} does not exist. The option are credit_g, spambase, vertebra_column, wine.'.format(dataset))
+
+    metadata_path = path.join(default_path, '{}.json'.format(dataset))
+    with open(metadata_path, 'r') as file:
+        metadata = json.load(file)
+
+    data_path = path.join(default_path, '{}.csv'.format(dataset))
+    data = read_csv(data_path, header=0)
+
+    target = None
+    numerical_features = []
+
+    for feature in metadata['features']:
+        if feature['type'] == 'numeric':
+            numerical_features.append(feature['name'])
+        elif feature['name'] == metadata['default_target_attribute']:
+            target = int(feature['index'])
+
+    x = concat([data.iloc[:, :target], data.iloc[:, target + 1:]], axis=1, sort=False)
+    y = data.iloc[:, target]
+
+    return numerical_features, x, y
 
 
 def __unique(values):
