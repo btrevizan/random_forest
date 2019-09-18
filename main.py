ce01ff3c84fbeb71a931e64587a01655b39b57e0
João <jpgmoreira19@gmail.com>
2019-09-18 18:39:08 -0300

    Test with values of y: different values for same seed.

--- main.py
+++ main.py
@@ -12,10 +12,15 @@ data = utils.load(dataset)
 x = data[1]  # table
 y = data[2]  # classes
 
+import sys
+
+print(y[0:10])
+sys.exit()
+
 #
 
-# random_state = numpy.random.RandomState(seed)
-# random_forest = rf.RandomForest(ntrees, random_state, x, y)
+random_state = numpy.random.RandomState(seed)
+random_forest = rf.RandomForest(ntrees, random_state, x, y)
 
 
 
--- src/dtree.py
+++ src/dtree.py
@@ -1,4 +1,7 @@
 import math
+import collections
+import numpy
+import sys
 
 # Attribute types:
 #   float64
@@ -8,18 +11,39 @@ import math
 #   bool
 #   object (string)
 
-def compute_information_gain(attribute, x, y):
-	return 1
 
+
+def compute_information_gain(attribute, x, y):
+	"""
+	attribute: integer. Index of the attribute in x to compute information gain.
+	"""
+
+
+def compute_entropy(y):
+	""" 
+	y: Class vector.
+	"""
+	total_instances = len(y)
+	entropy = 0.0
+	for k, v in collections.Counter(y).items():
+		pk = v / total_instances
+		entropy += pk * numpy.log2(pk)
+	entropy *= -1
+	return entropy
 
 class DTree:
-	def __init__(self, x, y, random_state, possible_attributes):
+	"""
+	bootstrap: List of integers, indexes of the instances selected from x.
+	possible_attributes: List of integers, indexes of the possible attributes in x for the node.
+	"""
+	def __init__(self, bootstrap, x, y, random_state, possible_attributes):
 		number_attributes_choose = math.ceil(math.sqrt(len(possible_attributes)))
 		attributes_choose = list(random_state.choice(possible_attributes, number_attributes_choose, replace = False))
 
 		best_info_gain = -math.inf
+		selected_attribute = None		
 
-		selected_attribute = None
+		entropy = compute_entropy(y)
 
 		for attribute in attributes_choose:
 			info_gain = compute_information_gain(attribute, x, y)
