fc55e2045102d81fc948672a83cb284c8bd717eb
Bernardo Trevizan <trevizanbernardo@gmail.com>
2019-09-20 23:10:56 -0300

    Improve instance creation

--- src/random_forest.py
+++ src/random_forest.py
@@ -1,34 +1,29 @@
 from src.dtree import DecisionTree
+from threading import Thread
 import src.utils as utils
 from .model import Model
 from copy import copy
+import os
 
 
 class RandomForest(Model):
 	"""Random forest representation."""
 
 	def __init__(self, ntrees, random_state, numerical_attributes_indexes=[]):
-		self.__trees = []
 		self.__ntrees = ntrees
 		self.__random_state = random_state
 		self.__numerical_attributes_indexes = numerical_attributes_indexes
+		self.__trees = [DecisionTree(self.__random_state, self.__numerical_attributes_indexes) for _ in range(self.__ntrees)]
 
 	@property
 	def trees(self):
 		return copy(self.__trees)
 
 	def fit(self, x, y) -> None:
-
+		
 		for i in range(self.__ntrees):
 			bootstrap = utils.bootstrap(x.shape[0], self.__random_state)
-
-			tree = DecisionTree(self.__random_state, self.__numerical_attributes_indexes)
-			tree.fit(x[bootstrap, :], y[bootstrap])
-
-			if len(self.__trees) == self.__ntrees:
-				self.__trees[i] = tree
-			else:
-				self.__trees.append(tree)
+			self.__trees[i].fit(x[bootstrap, :], y[bootstrap])
 
 	def predict(self, x) -> list:
 		if len(self.__trees) == 0:
