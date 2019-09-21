98394f1f9cf72815aceac00a3b70aea33b0d8bc6
Bernardo Trevizan <trevizanbernardo@gmail.com>
2019-09-20 21:53:06 -0300

    Test main

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import numpy
-import src.random_forest as rf
-import src.dtree as dtree
+from src.random_forest import RandomForest
 import src.utils as utils
+import numpy as np
 import graphviz
 
 seed = 0
@@ -10,23 +9,20 @@ dataset = 'credit_g'
 
 #
 
-data = utils.load(dataset)
-x = data[1]  # table
-y = data[2]  # classes
-
-data[3].remove('class')
-dtree.all_attribute_names = data[3]
+numerical_attributes, attr_names, x, y = utils.load(dataset)
 
 #
 
-random_state = numpy.random.RandomState(seed)
-random_forest = rf.RandomForest(ntrees, random_state, x, y, data[0])
+random_state = np.random.RandomState(seed)
+
+random_forest = RandomForest(ntrees, random_state, numerical_attributes)
+random_forest.fit(x, y)
 
 #
 
 for i in range(5):
 	dot = graphviz.Digraph(name=str(i))
-	random_forest.trees[i].get_graph(dot)
+	random_forest.trees[i].get_graph(dot, attr_names=attr_names)
 	dot.render(cleanup=True)
 
 #
