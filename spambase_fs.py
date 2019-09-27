172008e03f091127b5c04b846aa69789338e46f5
Bernardo Trevizan <trevizanbernardo@gmail.com>
2019-09-27 00:36:39 -0300

    Spambase feature selection

---
+++ spambase_fs.py
@@ -0,0 +1,20 @@
+"""Feature selection for spambase dataset."""
+from sklearn.feature_selection import RFE  # Recursive Feature Elimination (ranker)
+from pandas import read_csv, concat
+from sklearn.svm import SVC
+import numpy as np
+
+data = read_csv('tests/datasets/spambase.csv', header=[0], index_col=None)
+x, y = data.iloc[:, :-1], data.iloc[:, -1]
+
+estimator = SVC(kernel='linear', gamma='auto', random_state=np.random.RandomState(22))
+selector = RFE(estimator, 6)
+selector = selector.fit(x, y)
+
+selected_mask = selector.support_
+
+x = x.iloc[:, selected_mask]
+
+data = concat([x, y], axis=1)
+data.to_csv('tests/datasets/spambase_processed.csv', header=True, index=False)
+
