242522c2370803f1e2b73b64868a3c4ac9922a27
Bernardo Trevizan <trevizanbernardo@gmail.com>
2019-10-01 08:35:40 -0300

    Feature selection for Spambase using InfoGain

--- spambase_fs.py
+++ spambase_fs.py
@@ -1,20 +1,23 @@
 """Feature selection for spambase dataset."""
-from sklearn.feature_selection import RFE  # Recursive Feature Elimination (ranker)
-from pandas import read_csv, concat
-from sklearn.svm import SVC
-import numpy as np
+from src.utils import information_gain, entropy
+from pandas import read_csv, concat, DataFrame
 
-data = read_csv('tests/datasets/spambase.csv', header=[0], index_col=None)
-x, y = data.iloc[:, :-1], data.iloc[:, -1]
+data = read_csv('tests/datasets/spambase.csv', header=0, index_col=None)
+x, y = data.iloc[:, :-1].values, data.iloc[:, -1].values
+n, d = x.shape
 
-estimator = SVC(kernel='linear', gamma='auto', random_state=np.random.RandomState(22))
-selector = RFE(estimator)
-selector = selector.fit(x, y)
+gains = []
+class_entropy = entropy(y)
+for j in range(d):
+    info_gain = information_gain(class_entropy, x[:, j], y)
+    gains.append((j, info_gain))
 
-selected_mask = selector.support_
+gains.sort(key=lambda t: t[1], reverse=True)
+for gain in gains:
+    print(gain)
 
-x = x.iloc[:, selected_mask]
+js, _ = zip(*gains)
+js = list(js)
 
-data = concat([x, y], axis=1)
+data = concat([data.iloc[:, js[:10]], data.iloc[:, -1]], axis=1)
 data.to_csv('tests/datasets/spambase_processed.csv', header=True, index=False)
-
