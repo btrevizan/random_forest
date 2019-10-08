b2053df89bb7833cec29c0d1a509508b66e25600
Bernardo Trevizan <trevizanbernardo@gmail.com>
2019-10-08 09:58:30 -0300

    New graphics

--- graphics.py
+++ graphics.py
@@ -1,4 +1,4 @@
-from pandas import read_csv, DataFrame
+from pandas import read_csv, DataFrame, concat
 from matplotlib import pyplot as plt
 from src.utils import datasets
 from glob import glob
@@ -18,7 +18,7 @@ columns = ['Dataset', 'Mean F1-Score', 'Standard Deviation', 'Number of Trees',
 
 sns.set_style('whitegrid')
 
-for dataset in set(datasets) - {'benchmark'}:
+for dataset in set(datasets) - {'benchmark', 'spambase_processed'}:
     data = {columns[1]: [], columns[2]: [], columns[3]: [], columns[7]: []}
     dt_with_path = [f for f in glob(os.path.join(with_path, dataset, '*.csv')) if 'summary.csv' not in f]
     dt_without_path = [f for f in glob(os.path.join(without_path, dataset, '*.csv')) if 'summary.csv' not in f]
@@ -35,7 +35,7 @@ for dataset in set(datasets) - {'benchmark'}:
         data[columns[1]] += [np.mean(scores.flatten())]
         data[columns[2]] += [np.std(scores.flatten())]
         data[columns[3]] += [int(path.split('/')[-1][:-4])]
-        data[columns[7]] += ['Yes']
+        data[columns[7]] += ['No']
 
     data = DataFrame(data)
 
@@ -46,3 +46,31 @@ for dataset in set(datasets) - {'benchmark'}:
     graphic_path = os.path.join(graphics_path, dataset + '.pdf')
     plt.savefig(graphic_path, dpi=360, pad_inches=0.1, bbox_inches='tight')
     plt.close()
+
+data = {columns[1]: [], columns[2]: [], columns[3]: [], "Selected Features": []}
+dt_with_path = [f for f in glob(os.path.join(results_path, 'spambase_processed/*.csv')) if 'summary.csv' not in f]
+dt_without_path = [f for f in glob(os.path.join(results_path, 'spambase/*.csv')) if 'summary.csv' not in f]
+
+for path in dt_with_path:
+    scores = read_csv(path, header=0, index_col=None).values
+    data[columns[1]] += [np.mean(scores.flatten())]
+    data[columns[2]] += [np.std(scores.flatten())]
+    data[columns[3]] += [int(path.split('/')[-1][:-4])]
+    data["Selected Features"] += ['Yes']
+
+for path in dt_without_path:
+    scores = read_csv(path, header=0, index_col=None).values
+    data[columns[1]] += [np.mean(scores.flatten())]
+    data[columns[2]] += [np.std(scores.flatten())]
+    data[columns[3]] += [int(path.split('/')[-1][:-4])]
+    data["Selected Features"] += ['No']
+
+data = DataFrame(data)
+
+ax = sns.scatterplot(x=columns[3], y=columns[1], hue="Selected Features", size=columns[2], data=data, palette='winter')
+ax.legend(loc='lower right')
+# ax.set_title(dataset)
+
+graphic_path = os.path.join(graphics_path, 'spambase.pdf')
+plt.savefig(graphic_path, dpi=360, pad_inches=0.1, bbox_inches='tight')
+plt.close()
